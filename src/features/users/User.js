8b8ba2eadb525b3bb5b2731fa667a6ff6bdcb718
Tk21111 <101468538+Tk21111@users.noreply.github.com>
2024-06-16 14:26:09 +0700

    serch tag and text func in user

--- src/features/users/User.js
+++ src/features/users/User.js
@@ -31,15 +31,51 @@ const GetAllNoteUser = () => {
     if (isLoading) {
         content = <p>Loading...</p>;
     } else if (isSuccess) {
+        function filterEntitiesByTag(entities, tagsToFilter, x) {
+            let filteredEntities = {};
+            
+            try {
+                if (!Array.isArray(tagsToFilter)) {
+                    tagsToFilter = [tagsToFilter];
+                }
+            
+                for (let key in entities) {
+                    if (entities.hasOwnProperty(key)) {
+                        let entity = entities[key];
+                        if (Array.isArray(entity[x])){
+                            if (entity[x] && entity[x].some(tag => tagsToFilter.includes(tag))) {
+                                filteredEntities[key] = entity;
+                            }
+                        } else {
+                            if (entity[x] && tagsToFilter.includes(entity[x]))
+                            filteredEntities[key] = entity
+                        }
+                        
+                    }
+                }
+            
+            } catch (err) {
+                console.log(err)
+            }
+            
+            return filteredEntities;
+        }
+        
+        const serch = 'pork'
+        const type = 'text'
 
         //this is what u have to modifine
-        //user = {data : ids , entities}
-
-        if(users.ids.length !== 0){
+        //user = { ids , entities}
+        console.log(users)
+        if(users.ids.length !== 0 && !serch ){
             content = users.ids.map(postId => <PostsExcerpt key={postId} postId={postId} />)
-        } else 
+        } else if (serch) {
+            const f = filterEntitiesByTag(users.entities , serch , type)
+            const key = Object.keys(f)
+            content = key.map(postId => <PostsExcerpt key={postId} postId={postId} />)
+        } else {
             content = <Link to="/user/note/create">Create yours own note</Link>
-        
+        }
         
     } else if (isError) {
         let msg;
