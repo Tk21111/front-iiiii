b8afe8a0a2319fdd4a9a2f782508280cfc84d412
Tk21111 <101468538+Tk21111@users.noreply.github.com>
2024-06-16 13:18:31 +0700

    public page

--- src/components/Public.js
+++ src/components/Public.js
@@ -5,14 +5,10 @@ const Public = () => {
     const content = (
         <section className="public">
             <header>
-                <h1>Welcome to randomNumber page for something Christmas ig.</h1>
+                <h1>Welcome to food storage web.</h1>
             </header>
             <main>
-                <p>This have been plan over a year in advance 
-
-                    now it ready to use maybe we'll see what gonna happen
-                    
-                    ps. if u have any problem with unauthorized page just go back i'm tooo lazy to build verified role in frontend 
+                <p>PLS PAY SO IT WORK
                 </p>
                 <p2>btw i don't know how CSS work so don't mind me</p2>
             </main>
