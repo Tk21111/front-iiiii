6e5a277eb566035da80683a213ba2c3a381cc9c5
Tk21111 <101468538+Tk21111@users.noreply.github.com>
2024-11-13 10:45:30 +0000

    page change

--- src/features/users/User.js
+++ src/features/users/User.js
@@ -1,25 +1,35 @@
 import { useEffect, useState } from "react";
-import { selectAllNotes, selectNoteById, useGetAllNoteUserMutation } from "./NoteApiSlice";
+import {
+    selectAllNotes,
+    selectNoteById,
+    useGetAllNoteUserMutation,
+} from "./NoteApiSlice";
 import { useSelector } from "react-redux";
 import { selectCurrentUser } from "../auth/authSlice";
 import PostsExcerpt from "./NotesExcerpt";
 import filterEntitiesByTag from "./comp/Search";
 
-import ReactDom from 'react-dom';
+import ReactDom from "react-dom";
 
 import { Link, useParams } from "react-router-dom";
 import Header from "../../components/Header";
 import Overay from "../../components/Overlay";
 
 const GetAllNoteUser = () => {
+    const [page, setPage] = useState(0);
 
- 
     const user = { username: useSelector(selectCurrentUser) };
-    const [getAllNoteUser , { data: users, isLoading, isSuccess, isError, error }] = useGetAllNoteUserMutation(('noteUser', {
-        pollingInterval: 15000,
-        refetchOnFocus: true,
-        refetchOnMountOrArgChange: true
-    }));
+    const [
+        getAllNoteUser,
+        { data: users, isLoading, isSuccess, isError, error },
+    ] = useGetAllNoteUserMutation(
+        ("noteUser",
+        {
+            pollingInterval: 15000,
+            refetchOnFocus: true,
+            refetchOnMountOrArgChange: true,
+        }),
+    );
     const [hasFetched, setHasFetched] = useState(false);
 
     const [search, setSearch] = useState("");
@@ -29,10 +39,9 @@ const GetAllNoteUser = () => {
         const fetchData = async () => {
             try {
                 if (!hasFetched) {
-                    
                     await getAllNoteUser(user);
                     setHasFetched(true);
-                    console.log('f')
+                    console.log("f");
                 }
             } catch (error) {
                 console.error("Error fetching data:", error);
@@ -40,12 +49,9 @@ const GetAllNoteUser = () => {
         };
 
         fetchData();
-        
-    }, [ hasFetched]);
+    }, [hasFetched]);
 
     let content;
-
-  
 
     if (isLoading) {
         content = <p>Loading...</p>;
@@ -53,23 +59,23 @@ const GetAllNoteUser = () => {
         //this is what u have to modifine
         //user = { ids , entities}
         //content is list because str didn't work
-        content = []
+        content = [];
 
         if (users.ids.length !== 0 && !search) {
-            
             for (let i of users.ids) {
                 content.push(<PostsExcerpt key={i} i={users.entities[i]} />);
             }
-            console.log(content)
-            //content = users.ids.map(postId => <PostsExcerpt key={postId} postId={postId} />)
         } else if (search) {
-            for (let i of Object.keys(filterEntitiesByTag(users.entities, search, searchType))) {
+            for (let i of Object.keys(
+                filterEntitiesByTag(users.entities, search, searchType),
+            )) {
                 content.push(<PostsExcerpt key={i} i={users.entities[i]} />);
             }
             console.log(content);
         } else {
             content = <Link to="/user/note/create">Create yours own note</Link>;
         }
+        content = content?.splice(24 * page, 24 * (page + 1));
     } else if (isError) {
         let msg;
         if (error.status === 403) {
@@ -85,40 +91,53 @@ const GetAllNoteUser = () => {
         );
     }
 
-    console.log(useSelector(selectNoteById))
-
-        return (
-            <div className="page">
-                <Header />
-                <Overay link="user/note/create" />
-                <div className='content' >
-                    <img src={require('../../components/img/star.png')} alt="star" className="smalllogo"/>
-                    <p style={{ marginRight : 'auto'}} className='welcomefont'><Link to="/user" >Food Waste List</Link></p>
-                </div>
-                <div className="food-waste-list">
+    return (
+        <div className="page">
+            <Header />
+            <Overay link="user/note/create" />
+            <div className="content">
+                <img
+                    src={require("../../components/img/star.png")}
+                    alt="star"
+                    className="smalllogo"
+                />
+                <p style={{ marginRight: "auto" }} className="welcomefont">
+                    <Link to="/user">Food Waste List</Link>
+                </p>
+            </div>
+            <div className="food-waste-list">
                 {/* search comp*/}
                 <div className="search">
-                    <img src={require('../../components/img/search.png')} alt="icon" style={{marginLeft : '8px'}}/>
-                    <input type="text"
-                            value={search}
-                            onChange={(e) => setSearch(e.target.value)}/>
+                    <img
+                        src={require("../../components/img/search.png")}
+                        alt="icon"
+                        style={{ marginLeft: "8px" }}
+                    />
+                    <input
+                        type="text"
+                        value={search}
+                        onChange={(e) => setSearch(e.target.value)}
+                    />
                 </div>
-                    <select
-                        className="selection"
-                        value={searchType}
-                        onChange={(e) => setSearchType(e.target.value)}
-                        
-                    >   
-                        <option value="text">Text</option>
-                        <option value="tag">Tag</option>
-                    </select>
-                </div>
-                {/*end search comp*/}
-                <div className="user-list-parent">{content}</div>
-                
+                <select
+                    className="selection"
+                    value={searchType}
+                    onChange={(e) => setSearchType(e.target.value)}
+                >
+                    <option value="text">Text</option>
+                    <option value="tag">Tag</option>
+                </select>
             </div>
-        );
-   
+            {/*end search comp*/}
+            <div className="user-list-parent">{content}</div>
+            <div className="page-controll">
+                <button onClick={() => setPage(page - 1)} disabled={page === 0}>
+                    Previous
+                </button>
+                <button onClick={() => setPage(page + 1)}> next</button>
+            </div>
+        </div>
+    );
 };
 
 export default GetAllNoteUser;
