f5f0ecb7348ab4214be9ec818d87c39feaa30263
Tk21111 <101468538+Tk21111@users.noreply.github.com>
2024-11-14 09:51:30 +0700

    Update LocaOrgSingle.js

--- src/features/loca/LocaOrgSingle.js
+++ src/features/loca/LocaOrgSingle.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState , useRef} from 'react';
 import { useGetAlllocaQuery } from './LocaApiSlice';
 import {useSelector} from 'react-redux'
 import { selectCurrentUser } from '../auth/authSlice'
